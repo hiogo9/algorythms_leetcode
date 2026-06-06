9b3d95e8f523e8513bdc9c10d257124f82650b9e
rgusev <rodion@zinasoft.com>
2026-06-06 21:01:14 +0300

    add ruff and some linter and typing check disables

--- problems/2114_maximum_number_of_words_found_in_sentences/solution.py
+++ problems/2114_maximum_number_of_words_found_in_sentences/solution.py
@@ -4,14 +4,17 @@
 # [2114] Maximum Number of Words Found in Sentences
 #
 from typing import *
+
+
 # @lc code=start
 class Solution:
     def mostWordsFound(self, sentences: List[str]) -> int:
         ans = 0
         for s in sentences:
             cur_len = len(s.split(" "))
-            if cur_len > ans: 
+            if cur_len > ans:
                 ans = cur_len
         return ans
-# @lc code=end
 
+
+# @lc code=end
