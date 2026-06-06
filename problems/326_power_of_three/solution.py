c00048fe45aea62e99de74fdda0e31cc9cc9caa9
rgusev <rodion@zinasoft.com>
2026-06-06 21:51:14 +0300

    326 lazy fix for float in int

--- problems/326_power_of_three/solution.py
+++ problems/326_power_of_three/solution.py
@@ -8,7 +8,7 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         while n > 1:
-            n = n / 3
+            n = n / 3  # type: ignore cos im lazy shit
         if n == 1:
             return True
         else:
